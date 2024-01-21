446fc26fdb0ee2ef048242af74ffd709cd3467ba
Lymmak <mlymar-ki22@stud.sfu-kras.ru>
2024-01-22 00:02:19 +0700

    3. all ready to use

--- main.py
+++ main.py
@@ -12,4 +12,20 @@ def multiply(x, y):
     return x * y
 
 if __name__ == '__main__':
-    pass
+    print("Выберите операцию:")
+    print("1. Сложение")
+    print("2. Вычитание")
+    print("3. Умножение")
+
+    choice = input("Введите выбор (1/2/3): ")
+
+    num1, num2 = get_numbers()
+
+    if choice == '1':
+        print("Результат сложения:", add(num1, num2))
+    elif choice == '2':
+        print("Результат вычитания:", subtract(num1, num2))
+    elif choice == '3':
+        print("Результат умножения:", multiply(num1, num2))
+    else:
+        print("Введен недопустимый выбор")
