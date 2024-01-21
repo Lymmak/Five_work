e07dabc9720b7ae5faf2b2f261bdb21fa8d67c69
Lymmak <mlymar-ki22@stud.sfu-kras.ru>
2024-01-22 00:01:47 +0700

    2. func all ready to use main don't ready

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 def get_numbers():
-    pass
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+    return num1, num2
+def add(x, y):
+    return x + y
 
-def add():
-    pass
+def subtract(x, y):
+    return x - y
 
-def minus():
-    pass
-
-def mul():
-    pass
-
+def multiply(x, y):
+    return x * y
 
 if __name__ == '__main__':
     pass
